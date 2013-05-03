66ecc0e99b11c11ad7db1e1608dbeac6339158c5
Augusto QM <augustoqmacedo@gmail.com>
2013-05-03 19:44:32 -0300

    Script de Analise de constraints corrigido.

--- src/sqlCode/AnalyzeTableConstraints.sql
+++ src/sqlCode/AnalyzeTableConstraints.sql
@@ -1,9 +1,7 @@
 -- Analyze the constraints after loading the data
 select ANALYZE_CONSTRAINTS('time_dim');
 select ANALYZE_CONSTRAINTS('vm_dim');
-
--- Unnecesary checks because of these PKs are AUTO_INCREMENT fields
--- select ANALYZE_CONSTRAINTS('network');
--- select ANALYZE_CONSTRAINTS('disk');
--- select ANALYZE_CONSTRAINTS('cpu');
--- select ANALYZE_CONSTRAINTS('memory');
+select ANALYZE_CONSTRAINTS('network');
+select ANALYZE_CONSTRAINTS('disk');
+select ANALYZE_CONSTRAINTS('cpu');
+select ANALYZE_CONSTRAINTS('memory');
