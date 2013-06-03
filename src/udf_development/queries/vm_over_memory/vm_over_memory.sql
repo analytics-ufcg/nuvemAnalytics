0b445f29127bd50fb170b77e2b68c7b41e330b82
Iury Gregory <iurygregory@gmail.com>
2013-06-03 17:53:30 -0300

    Sprint 4 - Implementacao de Consulta = OK

--- src/udf_development/queries/vm_over_memory/vm_over_memory.sql
+++ src/udf_development/queries/vm_over_memory/vm_over_memory.sql
@@ -11,8 +11,8 @@ AS LANGUAGE 'R' NAME 'VMOverMemoryFuncFactory' LIBRARY nuvlib;
 -- PARAMETERS
 \set start_date '\'2007-01-31 08:15:00\''
 \set final_date '\'2010-01-31 08:15:00\''
-\set max_mem_util 10
-\set min_mem_alloc 5
+\set max_mem_util 0.35		-- Alternative value: 0.95
+\set min_mem_alloc 0.25		-- Alternative value: 0.15
 
 SELECT VMOverMemoryFunc(V.id_vm, V.vm_name, M.mem_util, M.mem_alloc, T.date_time, 
 			cast(:start_date as DATETIME), cast(:final_date as DATETIME), :max_mem_util, :min_mem_alloc) 
