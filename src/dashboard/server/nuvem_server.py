476d904e5849f24746f097962a9f91d79cfcb087
Guilherme Baptista <guisgb13@gmaail.com>
2013-06-05 17:21:40 -0300

    Turning my client script into a resuable module and having the flask server use it to query Vertica db

--- src/dashboard/server/nuvem_server.py
+++ src/dashboard/server/nuvem_server.py
@@ -1,11 +1,39 @@
-import  os
+import  os, sys
 from flask import Flask, render_template
+
+sys.path.append("../client")
+from vertica_query import VerticaClientFacade
 
 server = Flask(__name__)
 
 @server.route('/')
 def index():
+
 	return render_template("index.html")
+
+@server.route('/query/<query_identifier>?start_date=<start_date>&end_date=<end_date>')
+def do_query(query_identifier=None, start_date=None, end_date=None):
+
+	client = VerticaClientFacade()
+	(exit_status, message, output) = client.check_and_query(query_identifier, start_date, end_date)
+
+	if ( exit_status != 0 ):
+		return "too bad(%s): %s" % (exit_status, message)
+
+	return_page = ""
+
+	header = ""
+	for i in range(len(output.column_names)):
+		header += "%s (%s) \t\t" % (output.column_names[i]['name'], output.column_names[i]['measurement'])
+	return_page += header + "\n"
+
+	for row in output.rows:
+		formatted_row = ""
+		for i in range(len(row)):
+			formatted_row += row[i] + "\t\t"
+		return_page += formatted_row + "\n"
+
+	return return_page
 
 if __name__ == "__main__":
 
