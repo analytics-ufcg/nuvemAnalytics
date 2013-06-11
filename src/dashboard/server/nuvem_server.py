8378844ed996e1852f853b3d52de45f32dd7ee08
Guilherme Santos <guisgb13@gmail.com>
2013-06-11 19:54:03 -0300

    US 25

--- src/dashboard/server/nuvem_server.py
+++ src/dashboard/server/nuvem_server.py
@@ -6,27 +6,17 @@ from vertica_query import VerticaClientFacade
 
 server = Flask(__name__)
 
-@server.route('/')
-def index():
-
-	return render_template("index.html")
-
-@server.route('/query/<query_identifier>/<start_date>/<end_date>')
-def do_query(query_identifier=None, start_date=None, end_date=None):
+def execute_query(query_identifier, start_date, end_date, response):
 
 	client = VerticaClientFacade()
-	(exit_status, message, output) = client.check_and_query(query_identifier, start_date, end_date)
 
-	response = { 
-		'name' : 'subutilization',
-		'children' : [],
-		'exit_status' : exit_status,
-		'message' : message
-	}
+	(exit_status, message, output) = client.check_and_query(query_identifier, start_date, end_date)
 
 	if ( exit_status != 0 ):
 
-		return render_template("index.html", response=json.dumps(response))
+		response['exit_status'] = exit_status
+		response['message'] = message
+		return
 
 	query_results = {
 		'name' : query_identifier,
@@ -34,15 +24,59 @@ def do_query(query_identifier=None, start_date=None, end_date=None):
 	}
 
 	for row in output.rows:
+
 		vm_info = {'size' : 1}
+		
 		for i in range(0, len(output.column_names)):
+		
 			vm_info[output.column_names[i]['name']] = {
-			'value' : row[i],
-			'measurement' : output.column_names[i]['measurement']
+				'value' : row[i],
+				'measurement' : output.column_names[i]['measurement']
 			}
+
 		query_results['children'].append(vm_info)
 
-	response['response'].append(query_results)
+	response['children'].append(query_results)
+
+
+@server.route('/')
+@server.route('/query/<query_identifier>/<start_date>/<end_date>')
+def do_query(query_identifier=None, start_date=None, end_date=None):
+
+	if query_identifier == None or start_date == None or end_date == None:
+		return render_template("index.html")
+
+	response = { 
+		'name' : query_identifier,
+		'children' : [],
+		'exit_status' : exit_status,
+		'message' : message
+	}
+
+	execute_query(query_identifier, start_date, end_date, response)
+	if response['exit_status'] != 0:
+		pass #flash
+
+	return render_template("index.html", response=json.dumps(response))
+
+@server.route('/subutilization/<start_date>/<end_date>')
+def do_subutilization_queries(start_date=None, end_date=None):
+
+	response = { 
+		'name' : 'subutilization',
+		'children' : [],
+		'exit_status' : -1,
+		'message' : ""
+	}
+
+	execute_query("vmsOverMemAlloc", start_date, end_date, response)
+	if response['exit_status'] != 0:
+		pass #flash
+
+	execute_query("lowUsageVMs", start_date, end_date, response)
+	if response['exit_status'] != 0:
+		pass #flash
+
 	return render_template("index.html", response=json.dumps(response))
 
 if __name__ == "__main__":
