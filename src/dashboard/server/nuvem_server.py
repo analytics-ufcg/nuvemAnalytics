96a949ba0cf76ded919731a779eefcef4a9744b6
Nuvem Analytics <nuvem@ubuntu.(none)>
2013-06-17 18:04:10 -0300

    new server with aggregation

--- src/dashboard/server/nuvem_server.py
+++ src/dashboard/server/nuvem_server.py
@@ -1,4 +1,4 @@
-import  os, sys, json
+import  os, sys, json, itertools, copy
 from flask import Flask, render_template
 
 sys.path.append("../client")
@@ -15,7 +15,7 @@ def execute_query(query_identifier, start_date, end_date, response):
 	response['exit_status'] = exit_status
 	response['message'] = message
 
-	if ( exit_status != 0 ):
+	if (exit_status != 0):
 		return
 
 	# Add the children conditionally just if there is any answer 
@@ -43,6 +43,68 @@ def execute_query(query_identifier, start_date, end_date, response):
 		response['children'].append(query_results)
 
 
+def aggregate_problems(start_date, end_date, response):
+	
+	# Get the query_names, vm_names and query_vm_info's
+	query_names = []
+	query_vms_map = {}
+	query_vms_info_map = {}
+	for child_query in response['children']:
+		query_names.append(child_query['name'])
+		
+		query_vms = []
+		for vm_info in child_query['children']:
+			query_vms.append(vm_info['name'])
+		query_vms_map[child_query] = query_vms
+		
+		query_vms_info_map[child_query] = child_query['children']
+
+		response = { 
+		'name' : '',  # Remember to add the 'subutilization' then...
+		'children' : []
+	}
+	
+	combs = []
+	for i in range(len(query_names), 0, -1):
+		combs.extend([list(x) for x in itertools.combinations(query_names, i)])
+		
+	for comb in combs:
+		print comb
+		vm_comb = []
+		
+		# Query Intersection 
+		for i in xrange(0, len(comb)):
+			if len(vm_comb) > 0:
+				vm_comb = list(set(vm_comb) & set(query_vms_map[comb[i]]))
+			else:
+				vm_comb.extend(query_vms_map[comb[i]])
+
+
+		# Remove the VMs from the initial query map
+		for vm in vm_comb:
+			for i in xrange(0, len(comb)):
+				query_vms_map[comb[i]].remove(vm)
+				
+		print vm_comb
+		
+		# Create the new response
+		if len(vm_comb) > 0:
+			query_results = {
+				'name' : ' - '.join(comb),
+				'children' : []
+			}
+	
+			for vm in vm_comb:
+				for info in query_vms_info_map[comb[0]]:
+					if info['name'] == vm:
+						query_results['children'].append(info)
+						break;
+				
+			response['children'].append(query_results)
+		
+		print
+
+
 @server.route('/')
 def index():
 	return render_template("index.html")
@@ -51,21 +113,24 @@ def index():
 def do_subutilization_queries(start_date=None, end_date=None):
 
 	response = { 
-		'name' : '', # Remember to add the 'subutilization' then...
+		'name' : '',  # Remember to add the 'subutilization' then...
 		'children' : []
 	}
 
 	execute_query("vmsOverMemAlloc", start_date, end_date, response)
 	if response['exit_status'] != 0:
-		pass #flash this error
+		pass  # flash this error
 
 	execute_query("vmsOverCPU", start_date, end_date, response)
 	if response['exit_status'] != 0:
-		pass #flash this error
+		pass  # flash this error
 	
 	execute_query("lowUsageVMs", start_date, end_date, response)
 	if response['exit_status'] != 0:
-		pass #flash this error
+		pass  # flash this error
+
+	# Aggregate problems
+	aggregate_problems(start_date, end_date, response)
 
 	return render_template("index.html", response=json.dumps(response))
 
@@ -73,15 +138,16 @@ def do_subutilization_queries(start_date=None, end_date=None):
 def do_superutilization_queries(start_date=None, end_date=None):
 
 	response = { 
-		'name' : '', # Remember to add the 'subutilization' then...
+		'name' : '',  # Remember to add the 'superutilization' then...
 		'children' : []
 	}
 
 	execute_query("vmsNetConstrained", start_date, end_date, response)
 	if response['exit_status'] != 0:
-		pass #flash this error
+		pass  # flash this error
 
 	return render_template("index.html", response=json.dumps(response))
+
 
 if __name__ == "__main__":
 
--- src/sql_scripts/dml_scripts/queries/VMsNetConstrained.sql
+++ src/sql_scripts/dml_scripts/queries/VMsNetConstrained.sql
@@ -2,7 +2,7 @@ SELECT V.vm_name, Net.pkt_percentile
 FROM (SELECT DISTINCT N.id_vm, PERCENTILE_CONT(.90) WITHIN GROUP(ORDER BY N.pkt_per_sec) 
 					  		   OVER (PARTITION BY N.id_vm) AS pkt_percentile
 	  FROM vm_dim V, network N, time_dim T
-	  WHERE T.date_time >= '2012-06-01 00:00:00' and 
+	  WHERE T.date_time >= '2011-06-01 00:00:00' and 
 	  		T.date_time <= '2013-01-01 00:00:00' and 
 	  		N.id_time = T.id_time) as Net, 
 	  vm_dim V
